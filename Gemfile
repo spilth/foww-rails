3f5f8d47a342b6ff9f2d719bfa94fd9de8051177
Brian Kelly <polymonic@gmail.com>
2022-07-14 08:16:48 -0500

    Move/Charge use colored symbols from the game

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ group :development do
   # gem "spring"
 end
 
+
+gem "dartsass-rails", "~> 0.4.0"
--- app/helpers/units_helper.rb
+++ app/helpers/units_helper.rb
@@ -2,4 +2,16 @@ module UnitsHelper
   def distance_options
     Unit::DISTANCES.collect { |inches, color| ["#{inches} - #{color}", inches] }
   end
+
+  def move_icon(distance)
+    tag.span(class: "distance distance-#{distance}") do
+      tag.i(class: "fa-fw fa-solid fa-play distance-#{distance}")
+    end
+  end
+
+  def charge_icon(distance)
+    tag.span(class: "distance distance-#{distance}") do
+      tag.i(class: "fa-fw fa-solid fa-forward")
+    end
+  end
 end
