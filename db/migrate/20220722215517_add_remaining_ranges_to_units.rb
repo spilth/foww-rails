1a0f2042dd3060b8268bfbbcae00ecfb6b6ec01b
Brian Kelly <polymonic@gmail.com>
2022-08-07 17:29:25 -0500

    Fix past migration that got accidentally renamed

--- db/migrate/20220722215517_add_remaining_ranges_to_units.rb
+++ db/migrate/20220722215517_add_remaining_ranges_to_units.rb
@@ -1,7 +1,7 @@
 class AddRemainingRangesToUnits < ActiveRecord::Migration[7.0]
   def change
-    add_column :units, :throw_range_icon, :integer
-    add_column :units, :presence_range_icon, :integer
+    add_column :units, :throw_range, :integer
+    add_column :units, :presence_range, :integer
     add_column :units, :awareness_range, :integer
     add_column :units, :battle_cry_range, :integer
     add_column :units, :search_range, :integer
